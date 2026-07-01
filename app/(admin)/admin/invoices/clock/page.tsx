3a2b1dd4bd460740151be92c0fd120bfc6e30d82
mnsaify <mohammadnazifsaify20@gmail.com>
2026-07-01 18:08:03 -0400

    Remove mirror effect on camera

--- app/(admin)/admin/invoices/clock/page.tsx
+++ app/(admin)/admin/invoices/clock/page.tsx
@@ -285,11 +285,11 @@ export default function ClockPage() {
                         autoPlay
                         playsInline
                         muted
-                        style={{ width: '100%', height: '100%', objectFit: 'cover', transform: 'scaleX(-1)' }}
+                        style={{ width: '100%', height: '100%', objectFit: 'cover',  }}
                     />
                     <canvas 
                         ref={canvasRef} 
-                        style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', transform: 'scaleX(-1)' }} 
+                        style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%',  }} 
                     />
                 </div>
 
